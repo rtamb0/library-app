7ede02dd1a6e589bd2f7970cb18d8ab91e662cdd
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-28 18:10:15 +0700

    Create function that shows the library in the page

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const myLibrary = [];
 myLibrary.showLibrary = function() {
     this.forEach((book) => {
-        console.log(book);
+        const library = document.querySelector('.library');
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book');
+        library.appendChild(bookCard);
+        const titleText = document.createElement('h3');
+        titleText.innerHTML = `${book.title}`;
+        bookCard.appendChild(titleText);
+        const authorText = document.createElement('p');
+        authorText.innerHTML = `<strong>Made by:</strong> ${book.author}`;
+        bookCard.appendChild(authorText);
+        const pagesText = document.createElement('p');
+        pagesText.innerHTML = `<strong>Pages:</strong> ${book.pages}`;
+        bookCard.appendChild(pagesText);
+        const readText = document.createElement('p');
+        readText.innerHTML = `<strong>Read?</strong> ${book.read}`;
+        bookCard.appendChild(readText);
     });
 };
 
@@ -12,7 +27,7 @@ function Book(title, author, pages, read) {
     this.read = read
 };
 
-const book1 = new Book('20th Century Boys', 'Naoki Urasawa', '5000', 'In-Between');
+const book1 = new Book('20th Century Boys', 'Naoki Urasawa', '5000', 'No');
 myLibrary.push(book1);
 
 const book2 = new Book('Fate Stay/Night', 'TYPE-MOON', '10000', 'No');
@@ -22,3 +37,5 @@ function addBookToLibrary(title, author, pages, read) {
     const name = new Book(title, author, pages, read);
     myLibrary.push(name);
 };
+
+myLibrary.showLibrary();
