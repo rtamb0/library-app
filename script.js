77c1a845f9da1333ac7d20f7ac3d7f44dc1ee16c
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-28 16:40:47 +0700

    Add two example books + function to show the library

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const myLibrary = [];
+myLibrary.showLibrary = function() {
+    this.forEach((book) => {
+        console.log(book);
+    });
+};
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -6,6 +11,12 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
 };
+
+const book1 = new Book('20th Century Boys', 'Naoki Urasawa', '5000', 'In-Between');
+myLibrary.push(book1);
+
+const book2 = new Book('Fate Stay/Night', 'TYPE-MOON', '10000', 'No');
+myLibrary.push(book2);
 
 function addBookToLibrary(title, author, pages, read) {
     const name = new Book(title, author, pages, read);
